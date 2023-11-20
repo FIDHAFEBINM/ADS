af51d8fd8b2d28a76b30e2d13fb955871a53fd00
FIDHAFEBINM <148366775+FIDHAFEBINM@users.noreply.github.com>
2023-11-20 11:07:40 +0530

    Update circularqueue.c

--- circularqueue.c
+++ circularqueue.c
@@ -42,3 +42,48 @@ if((rear+1)%MAX==front){
 printf("queue is full");
 }
 else{
+if(front==-1)
+front=0;
+rear=(rear+1)%MAX;
+queue[rear]=element;
+}}
+void dequeue(){
+if(front==-1)
+{
+printf("\nqueue is empty\n");
+}
+else if(front==rear){
+printf("\nthe deleted element is:%d\n",queue[front]);
+front=rear=-1;}
+else{
+printf("\nthe deleted element is:%d\n",queue[front]);
+front=(front+1)%MAX;
+}
+}
+
+void display()
+{
+int front_pos=front,rear_pos=rear;
+if(front==-1)
+{
+printf("\nqueue is empty");
+}
+else{
+printf("\n......\n");
+
+if(front_pos>rear_pos)
+{
+while(front_pos>rear_pos)
+{
+printf("%d",queue[front_pos]);
+front_pos=((front_pos+1))%MAX;
+}
+}
+if (front_pos<=rear_pos){
+for(i=front_pos;i<=rear_pos;i++){
+printf("%d",queue[i]);
+}
+
+printf("\n....\n");}}}
+void search()
+
